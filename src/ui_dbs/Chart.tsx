9196487835865bf1903f3f8a1453346a416f4498
kitchens <kitchens@qq.com>
2021-12-06 16:13:29 +0800

    6666666

--- src/ui_dbs/Chart.tsx
+++ src/ui_dbs/Chart.tsx
@@ -7,23 +7,23 @@ export type Param = {
 }
 const Ui: FC<Param> = (props) => {
     const domId = `idColumn${props.uiId}`;
-    const data = [
-        { year: '2001', population: 41.8 },
-        { year: '2002', population: 38 },
-        { year: '2003', population: 33.7 },
-        { year: '2004', population: 30.7 },
-        { year: '2005', population: 25.8 },
-        { year: '2006', population: 31.7 },
-        { year: '2007', population: 33 },
-        { year: '2008', population: 46 },
-        { year: '2009', population: 38.3 },
-        { year: '2010', population: 28 },
-        { year: '2011', population: 42.5 },
-        { year: '2012', population: 30.3 },
-    ];
     useEffect(() => {
+        const data = [
+            { year: '2001', population: 41.8 },
+            { year: '2002', population: 38 },
+            { year: '2003', population: 33.7 },
+            { year: '2004', population: 30.7 },
+            { year: '2005', population: 25.8 },
+            { year: '2006', population: 31.7 },
+            { year: '2007', population: 33 },
+            { year: '2008', population: 46 },
+            { year: '2009', population: 38.3 },
+            { year: '2010', population: 28 },
+            { year: '2011', population: 42.5 },
+            { year: '2012', population: 30.3 },
+        ];
         const chart = new Chart({
-            container: 'container',
+            container: domId,
             autoFit: true,
             height: 500,
         });
